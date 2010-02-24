503cc663709df4c69a0caa306d666af1e383bd76
tiberich <tiberiuichim@users.noreply.github.com>
2010-02-24 14:30:36 +0000

    Fix DirectoryView registration; #3002

--- eea/gen.sh
+++ eea/gen.sh
@@ -1,5 +1,10 @@
 ./archgenxml -c archgenxml.cfg ims-ng.zuml 
 
+#rename Products.indicators to eea.indicators
 find indicators/* -type f -print | xargs sed -i 's/Products\.indicators/eea\.indicators/g'
 
-# -name “*.cpp” 
+#in the xml type profiles, replace indicators by eea.indicators
+find indicators/* -type f -print | xargs sed -i 's/>indicators</>eea\.indicators</g'
+
+#in the profiles.zcml, rename the profile to eea.indicators
+find indicators/* -type f -print | xargs sed -i 's/title=\"indicators\"/title=\"eea\.indicators\"/g'
