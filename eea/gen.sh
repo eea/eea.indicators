84ecc139981ed626eac44192ac16dc216920bdd1
tiberich <tiberiuichim@users.noreply.github.com>
2010-02-25 14:44:36 +0000

    Add content type icons from existing site

--- eea/gen.sh
+++ eea/gen.sh
@@ -8,3 +8,5 @@ find indicators/* -type f -print | xargs sed -i 's/>indicators</>eea\.indicators
 
 #in the profiles.zcml, rename the profile to eea.indicators
 find indicators/* -type f -print | xargs sed -i 's/title=\"indicators\"/title=\"eea\.indicators\"/g'
+
+cp -rf ./ ~/svn/eea.indicators/eea/ 
