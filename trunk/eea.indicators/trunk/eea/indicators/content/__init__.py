4abbb8f4c4d3dc6c641f891589ab3fe0b9f8aa7f
tiberich <tiberich@24821446-e7ea-0310-81ac-b189f4507b0b>
2010-03-05 14:27:32 +0000

    Fix several relations for ExternalData and Data; #3066

--- trunk/eea.indicators/trunk/eea/indicators/content/__init__.py
+++ trunk/eea.indicators/trunk/eea/indicators/content/__init__.py
@@ -44,7 +44,6 @@ import Specification
 import Assessment
 import RationaleReference
 import AssessmentPart
-import WorkItem
 import PolicyDocumentReference
 
 ##code-section init-module-footer #fill in your manual code here
