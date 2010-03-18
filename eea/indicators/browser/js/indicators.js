a43097b26002a47777577a96695dd3a96aa1d39e
tiberich <tiberiuichim@users.noreply.github.com>
2010-03-18 12:13:34 +0000

    Add js file to change the Kupu editor when editing specifications; #3112

---
+++ eea/indicators/browser/js/indicators.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+		// $(".kupu-fulleditor").();
+		$(".kupu-table").remove();
+		$('.kupu-image').remove();
+		// $(".kupu-tb-styles option[value='h2|']").remove();
+		// $(".kupu-tb-styles option[value='h3|']").remove();
+
+		// kupu.tools.ui.paraoptions = [["Mememe", 'h5|']]
+});
+
