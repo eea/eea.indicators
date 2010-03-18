e7bbde23132398ecd16d80c82bbb3d935c7bc716
tiberich <tiberiuichim@users.noreply.github.com>
2010-03-18 12:38:29 +0000

    Remove the headline options from the kupu syles dropdown; closes #3112

--- eea/indicators/browser/js/indicators.js
+++ eea/indicators/browser/js/indicators.js
@@ -1,10 +1,10 @@
+function change_kupu_styles(){
+		$(".kupu-tb-styles option[value='h2|']").remove();
+		$(".kupu-tb-styles option[value='h3|']").remove();
+}
 $(document).ready(function () {
-		// $(".kupu-fulleditor").();
 		$(".kupu-table").remove();
 		$('.kupu-image').remove();
-		// $(".kupu-tb-styles option[value='h2|']").remove();
-		// $(".kupu-tb-styles option[value='h3|']").remove();
-
-		// kupu.tools.ui.paraoptions = [["Mememe", 'h5|']]
+		setTimeout('change_kupu_styles()', '2000');
 });
 
