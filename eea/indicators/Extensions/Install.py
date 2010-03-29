77e1fb79f16f68bb21b5fe62e97ae84727fd3bbe
Alexandru Ghica <alec.ghica@eaudeweb.ro>
2010-03-29 09:12:40 +0000

    Fixed workflow manager role bypasses guards. #3053

--- eea/indicators/Extensions/Install.py
+++ eea/indicators/Extensions/Install.py
@@ -18,16 +18,25 @@ from Products.CMFCore.utils import getToolByName
 from eea.indicators.config import PROJECTNAME
 
 def install(self, reinstall=False):
-    """External Method to install indicators 
-    
+    """External Method to install indicators
+
     This method to install a product is kept, until something better will get
     part of Plones front end, which utilize portal_setup.
     """
     out = StringIO()
     print >> out, "Installation log of %s:" % PROJECTNAME
 
-    setuptool = getToolByName(self, 'portal_setup')   
+    # eea.indicators import steps
+    setuptool = getToolByName(self, 'portal_setup')
     importcontext = 'profile-eea.%s:default' % PROJECTNAME
     setuptool.setImportContext(importcontext)
     setuptool.runAllImportSteps()
+
+    # DCWorkflowDump doesn't yet support the 'manager_bypass'
+    wf_id = 'specification_workflow'
+    wf_tool = getToolByName(portal, 'portal_workflow')
+    if wf_id in wf_tool.objectIds():
+        wfobj = wf_tool.getWorkflowById(wf_id)
+        wfobj.manager_bypass = 1
+
     return out.getvalue()
