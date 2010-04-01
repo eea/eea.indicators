c17927b803c53ec772b3d03216209378fbe2ae15
tiberich <tiberich@24821446-e7ea-0310-81ac-b189f4507b0b>
2010-04-01 13:44:08 +0000

    Add generated classes for v1. content types; #3136

---
+++ trunk/eea.indicators/trunk/eea/indicators/content/IndicatorFactSheet.py
@@ -0,0 +1,160 @@
+# -*- coding: utf-8 -*-
+#
+# $Id$
+#
+# Copyright (c) 2010 by ['Tiberiu Ichim']
+# Generator: ArchGenXML 
+#            http://plone.org/products/archgenxml
+#
+# GNU General Public License (GPL)
+#
+
+__author__ = """Tiberiu Ichim <unknown>"""
+__docformat__ = 'plaintext'
+
+from AccessControl import ClassSecurityInfo
+from Products.Archetypes.atapi import *
+from zope.interface import implements
+import interfaces
+from Products.ATContentTypes.content.folder import ATFolder
+from Products.CMFDynamicViewFTI.browserdefault import BrowserDefaultMixin
+
+from eea.indicators.config import *
+
+# additional imports from tagged value 'import'
+from Products.ATContentTypes.content.folder import ATFolder, ATFolderSchema
+from Products.Archetypes.atapi import MultiSelectionWidget
+
+##code-section module-header #fill in your manual code here
+##/code-section module-header
+
+schema = Schema((
+
+    StringField(
+        name='title',
+        required_for_publication=True,
+        widget=StringField._properties['widget'](
+            label="Title",
+            label_msgid='indicators_label_title',
+            i18n_domain='indicators',
+        ),
+        schemata="default",
+        searchable=True,
+        required=True,
+        accessor="Title",
+    ),
+    TextField(
+        name='description',
+        widget=TextAreaWidget(
+            label="Description",
+            label_msgid='indicators_label_description',
+            i18n_domain='indicators',
+        ),
+    ),
+    TextField(
+        name='assessment',
+        allowable_content_types=('text/plain', 'text/structured', 'text/html', 'application/msword',),
+        widget=RichWidget(
+            label="More updates on",
+            label_msgid='indicators_label_assessment',
+            i18n_domain='indicators',
+        ),
+        default_output_type='text/html',
+        searchable=True,
+    ),
+    DateTimeField(
+        name='assessment_date',
+        widget=DateTimeField._properties['widget'](
+            label="Assessment Date",
+            label_msgid='indicators_label_assessment_date',
+            i18n_domain='indicators',
+        ),
+    ),
+    StringField(
+        name='source_code',
+        widget=StringField._properties['widget'](
+            label="Source code",
+            label_msgid='indicators_label_source_code',
+            i18n_domain='indicators',
+        ),
+    ),
+    StringField(
+        name='data_source_providers',
+        widget=StringField._properties['widget'](
+            label="Data Source Providers",
+            label_msgid='indicators_label_data_source_providers',
+            i18n_domain='indicators',
+        ),
+    ),
+    StringField(
+        name='EEA_responsible',
+        widget=StringField._properties['widget'](
+            label="Owner",
+            label_msgid='indicators_label_EEA_responsible',
+            i18n_domain='indicators',
+        ),
+    ),
+    StringField(
+        name='DEV_responsible',
+        widget=StringField._properties['widget'](
+            label="ID of manager user",
+            label_msgid='indicators_label_DEV_responsible',
+            i18n_domain='indicators',
+        ),
+    ),
+    TextField(
+        name='data_source_info',
+        widget=TextAreaWidget(
+            label="Data source info",
+            label_msgid='indicators_label_data_source_info',
+            i18n_domain='indicators',
+        ),
+    ),
+    TextField(
+        name='priority_data_flows',
+        widget=TextAreaWidget(
+            label="Priority data flows",
+            label_msgid='indicators_label_priority_data_flows',
+            i18n_domain='indicators',
+        ),
+    ),
+
+),
+)
+
+##code-section after-local-schema #fill in your manual code here
+##/code-section after-local-schema
+
+IndicatorFactSheet_schema = ATFolderSchema.copy() + \
+    getattr(ATFolder, 'schema', Schema(())).copy() + \
+    schema.copy()
+
+##code-section after-schema #fill in your manual code here
+##/code-section after-schema
+
+class IndicatorFactSheet(ATFolder, BrowserDefaultMixin):
+    """
+    """
+    security = ClassSecurityInfo()
+
+    implements(interfaces.IIndicatorFactSheet)
+
+    meta_type = 'IndicatorFactSheet'
+    _at_rename_after_creation = False
+
+    schema = IndicatorFactSheet_schema
+
+    ##code-section class-header #fill in your manual code here
+    ##/code-section class-header
+
+    # Methods
+
+
+registerType(IndicatorFactSheet, PROJECTNAME)
+# end of class IndicatorFactSheet
+
+##code-section module-footer #fill in your manual code here
+##/code-section module-footer
+
+
+
