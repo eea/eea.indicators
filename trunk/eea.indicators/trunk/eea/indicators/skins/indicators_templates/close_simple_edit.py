297e263aeeaf78a0970c486d6fac22fc4013d58e
tiberich <tiberich@24821446-e7ea-0310-81ac-b189f4507b0b>
2010-05-07 13:06:44 +0000

    Implement relations widget for Methodology References; #3207

--- trunk/eea.indicators/trunk/eea/indicators/skins/indicators_templates/close_simple_edit.py
+++ trunk/eea.indicators/trunk/eea/indicators/skins/indicators_templates/close_simple_edit.py
@@ -1,12 +1,21 @@
 field = context.REQUEST.form.get('specific_field')
+active_region = context.REQUEST.form.get('active_region');
+
 if not field:
     raise ValueError('We need a specific field here')
 
 #value = context.schema[field].getAccessor(context)()
 #<div id="value_response">%s</div> % value
 
-return """
+if active_region:
+    return """
 <script>
-closer('%s');
+closer('%s', '%s');
+</script>
+""" % (field, active_region); 
+else:
+    return """
+<script>
+closer('%s', null);
 </script>
 """ % field 
