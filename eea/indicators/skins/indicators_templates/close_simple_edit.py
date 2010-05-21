bdeff713ede537524f5ed3376c482816ee0d7123
tiberich <tiberiuichim@users.noreply.github.com>
2010-05-21 16:41:21 +0000

    Reload the page after the title is edited

--- eea/indicators/skins/indicators_templates/close_simple_edit.py
+++ eea/indicators/skins/indicators_templates/close_simple_edit.py
@@ -4,18 +4,17 @@ active_region = context.REQUEST.form.get('active_region');
 if not field:
     raise ValueError('We need a specific field here')
 
-#value = context.schema[field].getAccessor(context)()
-#<div id="value_response">%s</div> % value
+url = context.absolute_url() + '/edit_aggregated';
 
 if active_region:
     return """
 <script>
-closer('%s', '%s');
+closer('%s', '%s', '%s');
 </script>
-""" % (field, active_region); 
+""" % (field, active_region, url); 
 else:
     return """
 <script>
-closer('%s', null);
+closer('%s', null, '%s');
 </script>
-""" % field 
+""" % (field, url)
