a3d6f3f147379b31ac6720aea8e54c4c67fd3f35
Alexandru Ghica <alec.ghica@eaudeweb.ro>
2010-06-08 14:00:19 +0000

    Imgview adaptor for IndicatorFactSheet. #3417

--- eea/indicators/browser/imgview.py
+++ eea/indicators/browser/imgview.py
@@ -41,3 +41,33 @@ class ImageViewAssessment(BrowserView):
         if self.display(scalename):
             return self.img(scalename)
         raise NotFound(self.request, scalename)
+
+class ImageViewIndicatorFactSheet(BrowserView):
+    """ Get cover image from related EEAFigure
+    """
+    implements(IImageView)
+
+    def __init__(self, context, request):
+        wftool = getToolByName(context, 'portal_workflow')
+
+        super(ImageViewIndicatorFactSheet, self).__init__(context, request)
+
+        eeafile = None
+        for rel_ob in context.getRelatedItems():
+            if rel_ob.portal_type == 'EEAFigure':
+                state = wftool.getInfoFor(rel_ob, 'review_state', '(Unknown)')
+                if state in ['published', 'visible']:
+                    eeafile = rel_ob
+                    break
+
+        self.img = queryMultiAdapter((eeafile, request), name=u'imgview')
+
+    def display(self, scalename='thumb'):
+        if not self.img:
+            return False
+        return self.img.display(scalename)
+
+    def __call__(self, scalename='thumb'):
+        if self.display(scalename):
+            return self.img(scalename)
+        raise NotFound(self.request, scalename)
