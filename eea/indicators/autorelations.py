9bdbd7f4b40d93a951b82a3738cae6ed96759b75
Antonio De Marinis <demarinis@eea.europa.eu>
2010-06-25 10:29:12 +0000

    removed comments and cleanup a bit

--- eea/indicators/autorelations.py
+++ eea/indicators/autorelations.py
@@ -1,12 +1,5 @@
 from zope.component import getMultiAdapter                                                                                                                              
-#LinkChecker = getMultiAdapter((context, request),                                                                                                                      
-#                                   name=u'migrationLinkChecker')                                                                                                       
-#status_code = LinkChecker.getStatusCode(url)                                                                                                                           
-#    status_msg = LinkChecker.getStatusMsg(status_code)om zope.component import getMultiAdapter                                                                         
-#LinkChecker = getMultiAdapter((context, request),                                                                                                                      
-#                                   name=u'migrationLinkChecker')                                                                                                       
-#status_code = LinkChecker.getStatusCode(url)                                                                                                                           
-#    status_msg = LinkChecker.getStatusMsg(status_code)
+
 
 class DatasetsFromFigures(object):                                                                                                                                      
     """ Return datasets used in the latest figures of an assessment for a specification.                                                                                
@@ -18,11 +11,7 @@ class DatasetsFromFigures(object):
     def __call__(self, **kwargs):                                                                                                                                       
         """ Return all the related data sets from the assessments figures.                                                                                              
         """                                                                                                                                                             
-        #get my published assessments                                                                                                                                   
-        assessments = self.context.getFolderContents(                                                                                                                   
-                             contentFilter={'review_state':'published',                                                                                                 
-                                            'portal_type':'Assessment'},                                                                                                
-                             full_objects = True)                                                                                                                       
+        #get my published assessments                                                                                                                                                                                                                                                      
         assessments = getMultiAdapter((self.context, self.request),                                                                                                     
                                    name=u'assessment_versions')                                                                                                         
         all_assessments = assessments()                                                                                                                                 
@@ -44,3 +33,4 @@ class DatasetsFromFigures(object):
         #TODO: get any related data on each figure                                                                                                                      
         return [('Data used in figures',figs),                                                                                                                          
                 ('published assessments',published_assessments),] 
+
