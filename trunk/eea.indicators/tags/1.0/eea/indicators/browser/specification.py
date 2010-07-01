bed95291e5e7c3515925f8f3167f7cd9ee0d2bf3
ghicaale <ghicaale@24821446-e7ea-0310-81ac-b189f4507b0b>
2010-07-01 09:12:31 +0000

    Tagged eea.indicators 1.0.

---
+++ trunk/eea.indicators/tags/1.0/eea/indicators/browser/specification.py
@@ -0,0 +1,185 @@
+# -*- coding: utf-8 -*-
+
+__author__ = """European Environment Agency (EEA)"""
+__docformat__ = 'plaintext'
+__credits__ = """contributions: Alec Ghica, Tiberiu Ichim"""
+
+from DateTime import DateTime
+from Products.CMFCore.utils import getToolByName
+from Products.Five import BrowserView
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+from eea.versions.versions import create_version, CreateVersion as BaseCreateVersion
+from eea.workflow.readiness import ObjectReadinessView
+from eea.workflow.interfaces import IFieldIsRequiredForState, IValueProvider
+from zope.component import getMultiAdapter
+
+import logging
+logger = logging.getLogger('eea.indicators')
+
+class IndexPage(BrowserView):
+    """ """
+
+class AggregatedEditPage(BrowserView):
+    template = ViewPageTemplateFile('templates/specification/aggregated_edit.pt')
+
+    __call__ = template
+
+
+class SchemataCounts(BrowserView):
+    """Provides a dictionary of fields that are required for publishing grouped by schematas
+
+    TODO: see if able/worthy to move this to eea.workflow
+    """
+
+    def __call__(self):
+        schematas = {}
+        for field in self.context.schema.fields():
+            if not field.schemata in schematas:
+                schematas[field.schemata] = []
+            req = getMultiAdapter((self.context, field), IFieldIsRequiredForState)('published')
+            if req:
+                adapter = getMultiAdapter((self.context, field), IValueProvider)
+                if not adapter.has_value():
+                    schematas[field.schemata].append(field.__name__)
+
+        return schematas
+
+
+class CreateVersion(BaseCreateVersion):
+    """Create new version customizations for eea.versions """
+
+    def __call__(self):
+        
+        new_spec = create_version(self.context, False)
+        new_spec.setEffectiveDate(None)
+
+        #Delete assessments and work items
+        new_spec.manage_delObjects(ids=new_spec.objectIds('Assessment'))
+        new_spec.manage_delObjects(ids=new_spec.objectIds('WorkItem'))
+
+        #TODO: should we reindex the objects here?
+        for obj in new_spec.objectValues():
+            obj.setEffectiveDate(None)
+            obj.setCreationDate(DateTime())
+
+        new_spec.reindexObject()
+        self.context.reindexObject() #some indexed values of the context may depend on versions
+
+        return self.request.RESPONSE.redirect(new_spec.absolute_url())
+
+
+def has_one_of(has, in_list):
+    for obj in in_list:
+        if obj.meta_type in has:
+            return True
+    return False
+
+
+class WorkflowStateReadiness(ObjectReadinessView):
+
+    #TODO: translate messages here
+    checks = (
+            (
+                lambda o: not has_one_of(('EEAData', 'ExternalDataSpec'), o.getRelatedItems()),
+                "You need to point to at least one EEAData or ExternalData"),
+            (
+                lambda o:not bool(o.objectValues("PolicyQuestion")),
+                "You need to add at least one Policy Question"),
+            (
+                lambda o:not filter(lambda x:x.getIs_key_question(), o.objectValues('PolicyQuestion')),
+                "At least one PolicyQuestion needs to be main policy question"),
+            (
+                lambda o:not bool(o.getThemes()),
+                "You need to specify one primary theme" ),
+            )
+
+    def get_info_for(self, state_name):
+        info = ObjectReadinessView.get_info_for(self, state_name)
+        extras = []
+
+        for checker, error in self.checks:
+            if checker(self.context):
+                extras.append(('error', error))
+
+        info['extra'] = extras
+        return info
+
+    def is_ready_for(self, state_name):
+        if state_name == 'published':
+            for checker, error in self.checks:
+                if checker(self.context):
+                    return False
+                return True
+        else:
+            return super(WorkflowStateReadiness, self).is_ready_for(state_name)
+
+
+class AssessmentVersions(BrowserView):
+    """ Return contained Assessments divided by 'published' and 'draft' sorted
+        by publish_date and creation_date
+    """
+
+    def sort_assessments(self, data):
+        """ """
+        assessments = {}
+
+        for assessment in data:
+            try:
+                time = assessment.getEffectiveDate()
+                assessments[time] = assessment
+            except Exception, err:
+                logger.exception('Exception: %s ', err)
+
+        res = assessments.keys()
+        res.sort()
+        res.reverse()
+
+        return [assessments[k] for k in res]
+
+    def __call__(self):
+        res = {'published': [], 'draft': []}
+
+        assessments = self.context.getFolderContents(
+                             contentFilter={'review_state':'published',
+                                            'portal_type':'Assessment'},
+                             full_objects = True)
+        res['published'] = self.sort_assessments(assessments)
+
+        assessments = self.context.getFolderContents(
+                             contentFilter={'review_state':'draft',
+                                            'portal_type':'Assessment'},
+                             full_objects = True)
+        res['draft'] = self.sort_assessments(assessments)
+
+        return res
+
+
+class PolicyQuestions(BrowserView):
+    """ Return contained PolicyQuestions divided by 'is_key_question' property
+    """
+
+    def __call__(self):
+        res = {'all': [], 'key_questions': [], 'questions': []}
+
+        questions = self.context.getFolderContents(
+                           contentFilter={'portal_type':'PolicyQuestion'},
+                           full_objects = True)
+
+        res['all'] = questions
+        for question in questions:
+            if question.getIs_key_question():
+                res['key_questions'].append(question)
+            else:
+                res['questions'].append(question)
+
+        return res
+
+
+class ContactInfo(BrowserView):
+    """ Return LDAP user based on manager_user_id
+    """
+
+    def __call__(self):
+        manager_id = self.context.getManager_user_id()
+        mtool = getToolByName(self.context, 'portal_membership')
+        return mtool.getMemberInfo(manager_id)
---
+++ trunk/eea.indicators/tags/1.0/eea/indicators/content/PolicyQuestion.py
@@ -0,0 +1,105 @@
+# -*- coding: utf-8 -*-
+#
+# $Id$
+#
+# Copyright (c) 2010 by ['Tiberiu Ichim']
+# Generator: ArchGenXML 
+#            http://plone.org/products/archgenxml
+#
+# GNU General Public License (GPL)
+#
+
+__author__ = """Tiberiu Ichim <unknown>"""
+__docformat__ = 'plaintext'
+
+from AccessControl import ClassSecurityInfo
+from Products.Archetypes.atapi import *
+from zope.interface import implements
+import interfaces
+from Products.ATContentTypes.content.base import ATCTContent
+from Products.CMFDynamicViewFTI.browserdefault import BrowserDefaultMixin
+
+from eea.indicators.config import *
+
+# additional imports from tagged value 'import'
+from Products.ATContentTypes.content.base import ATCTContent, ATContentTypeSchema
+
+##code-section module-header #fill in your manual code here
+from Products.ATContentTypes.content.schemata import finalizeATCTSchema
+##/code-section module-header
+
+schema = Schema((
+
+    StringField(
+        name='title',
+        required_for_published=True,
+        widget=StringField._properties['widget'](
+            label="Question",
+            label_msgid='indicators_label_title',
+            i18n_domain='indicators',
+        ),
+        required=True,
+        accessor="Title",
+        searchable=True,
+    ),
+    BooleanField(
+        name='is_key_question',
+        widget=BooleanField._properties['widget'](
+            label="Is this a key question?",
+            label_msgid='indicators_label_is_key_question',
+            i18n_domain='indicators',
+        ),
+    ),
+    TextField(
+        name='description',
+        widget=TextAreaWidget(
+            visible={'view':'invisible', 'edit':'invisible'},
+            label='Description',
+            label_msgid='indicators_label_description',
+            i18n_domain='indicators',
+        ),
+        accessor="Description",
+        searchable=True,
+    ),
+
+),
+)
+
+##code-section after-local-schema #fill in your manual code here
+##/code-section after-local-schema
+
+PolicyQuestion_schema = ATContentTypeSchema.copy() + \
+    getattr(ATCTContent, 'schema', Schema(())).copy() + \
+    schema.copy()
+
+##code-section after-schema #fill in your manual code here
+finalizeATCTSchema(PolicyQuestion_schema)
+PolicyQuestion_schema['relatedItems'].widget.visible = {'view':'invisible', 'edit':'invisible'}
+##/code-section after-schema
+
+class PolicyQuestion(ATCTContent, BrowserDefaultMixin):
+    """
+    """
+    security = ClassSecurityInfo()
+
+    implements(interfaces.IPolicyQuestion)
+
+    meta_type = 'PolicyQuestion'
+    _at_rename_after_creation = True
+
+    schema = PolicyQuestion_schema
+
+    ##code-section class-header #fill in your manual code here
+    ##/code-section class-header
+
+    # Methods
+
+
+registerType(PolicyQuestion, PROJECTNAME)
+# end of class PolicyQuestion
+
+##code-section module-footer #fill in your manual code here
+##/code-section module-footer
+
+
+
