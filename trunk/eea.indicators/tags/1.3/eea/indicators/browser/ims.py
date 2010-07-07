a93506c59d8c8dfa9ce8b7adb1c73a754441432b
demarant <demarant@24821446-e7ea-0310-81ac-b189f4507b0b>
2010-07-07 07:44:48 +0000

    Tagged eea.indicators 1.3.

---
+++ trunk/eea.indicators/tags/1.3/eea/indicators/browser/ims.py
@@ -0,0 +1,125 @@
+from Products.CMFPlone.utils import getToolByName
+from Products.Five import BrowserView
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+from eea.indicators.browser.interfaces import IIndicatorsPermissionsOverview
+from zope.interface import implements
+
+
+#TODO: write test for this sorting
+def _get_code(set):
+    def _wrapped(info):
+        spec = info['spec']
+        codes = spec.getCodes()
+        for code in codes:
+            if set == code['set']:
+                return code['code']
+
+        return None
+    return _wrapped
+
+
+class IndicatorsOverview(BrowserView):
+    implements(IIndicatorsPermissionsOverview)
+
+    template = ViewPageTemplateFile('templates/ims_overview.pt')
+
+    __call__ = template
+
+    def get_setcodes_map(self):
+
+        result = {}
+        specs = self.context.objectValues("Specification")
+
+        wftool = getToolByName(self.context, 'portal_workflow')
+        get_state = lambda a:wftool.getInfoFor(a, 'review_state', '(Unknown)')
+
+        for spec in specs:
+            sets = [s['set'] for s in spec.getCodes()] 
+            if not sets:
+                sets = [None]
+            assessments = [(a, get_state(a)) for a in spec.objectValues("Assessment")]
+
+            for s in sets:
+                info = {
+                        'spec':spec,
+                        'manager_id':spec.getManager_user_id(),
+                        'state':wftool.getInfoFor(spec, 'review_state', '(Unknown)'),
+                        'assessments':assessments,
+                        }
+                if s in result.keys():
+                    result[s].append(info)
+                else:
+                    result[s] = [info]
+
+        for k, v in result.items():
+            v.sort(key=_get_code(k))
+
+        return result
+
+    def codeset_for(self, codes, setname):
+        codesets = [code for code in codes if code['set'] == setname]
+        if codesets:
+            codeset = codesets[0]
+        else:
+            codeset = {'set':'', 'code':''}
+
+        return codeset
+
+
+class IndicatorsTimeline(BrowserView):
+    """Presents a timeline based structure for Assessments
+    """
+    def get_timeline(self):
+        specs = self.context.objectValues("Specification")
+        wftool = getToolByName(self.context, 'portal_workflow')
+        get_state = lambda a:wftool.getInfoFor(a, 'review_state', '(Unknown)')
+
+        result = {
+                #example of data structure:
+                #'TERM':{
+                #    '001':{
+                #        '1998':[('n', None)], #no specification for that year
+                #        '1999':[('a', href)], #assessment at link href
+                #        'published':('p', href),    #assessment in Publication href
+                #        'future':[('f', href)],    #assessment has no effective date
+                #        }
+                #    }
+                }
+
+        earliest_year = 0
+        latest_year = 0
+        for spec in specs:
+            assessments = spec.objectValues("Assessment")
+            for codeset in spec.getCodes():
+                set, code = codeset['set'], codeset['code']
+                if not set in result:
+                    result[set] = {}
+
+                if not code in result[set]:
+                    result[set][code] = {'future':[],}
+
+                if not assessments:
+                    #TODO: see if there's a related EEAPublication to the Specification
+                    #pseudocode
+                    #if published(a):
+                    #    result[set][code]['p'] = [(p, p.absolute_url())]
+                    #else:
+                    result[set][code]['missing'] = [('m', spec.absolute_url())] + result[set][code].get('missing', [])
+
+                for a in assessments:
+                    d = a.getEffectiveDate()
+                    if not d:
+                        print "adding a for future", a
+                        result[set][code]['future'] = [('f', a.absolute_url())] + result[set][code].get('future', [])
+                        continue
+                    year = d.year()
+                    if year < earliest_year:
+                        earliest_year = year
+                    if year > latest_year:
+                        latest_year = year
+                        if earliest_year == 0:
+                            earliest_year = year
+
+                    result[set][code][year] = [('a', a.absolute_url())] + result[set][code].get(year, []) 
+
+        return ((earliest_year, latest_year), result)
---
+++ trunk/eea.indicators/tags/1.3/eea/indicators/content/WorkItem.py
@@ -0,0 +1,153 @@
+# -*- coding: utf-8 -*-
+#
+# $Id$
+#
+# Copyright (c) 2010 by ['Tiberiu Ichim']
+# Generator: ArchGenXML 
+#            http://plone.org/products/archgenxml
+#
+# GNU General Public License (GPL)
+#
+
+__author__ = """Tiberiu Ichim <unknown>"""
+__docformat__ = 'plaintext'
+
+from AccessControl import ClassSecurityInfo
+from Products.Archetypes.atapi import *
+from zope.interface import implements
+import interfaces
+from Products.ATContentTypes.content.base import ATCTContent
+from Products.CMFDynamicViewFTI.browserdefault import BrowserDefaultMixin
+
+from eea.indicators.config import *
+
+# additional imports from tagged value 'import'
+from Products.ATContentTypes.content.base import ATCTContent, ATContentTypeSchema
+
+##code-section module-header #fill in your manual code here
+from Products.CMFPlone.utils import getToolByName
+from Products.ATContentTypes.content.schemata import finalizeATCTSchema
+from eea.indicators import msg_factory as _
+##/code-section module-header
+
+schema = Schema((
+
+    StringField(
+        name='title',
+        widget=StringField._properties['widget'](
+            visible={'view':'invisible', 'edit':'invisible'},
+            label='Title',
+            label_msgid='indicators_label_title',
+            i18n_domain='indicators',
+        ),
+        searchable=False,
+    ),
+    TextField(
+        name='description',
+        allowable_content_types=('text/plain', 'text/structured', 'text/html', 'application/msword',),
+        widget=RichWidget(
+            label='Description',
+            label_msgid='indicators_label_description',
+            i18n_domain='indicators',
+        ),
+        default_content_type="text/html",
+        default_output_type="text/x-html-safe",
+        accessor="getDescription",
+    ),
+    TextField(
+        name='needs',
+        allowable_content_types=('text/plain', 'text/structured', 'text/html', 'application/msword',),
+        widget=RichWidget(
+            label='Needs',
+            label_msgid='indicators_label_needs',
+            i18n_domain='indicators',
+        ),
+        default_output_type='text/html',
+    ),
+    StringField(
+        name='status',
+        widget=SelectionWidget(
+            label='Status',
+            label_msgid='indicators_label_status',
+            i18n_domain='indicators',
+        ),
+        required=True,
+        vocabulary=["Not started", "In progress", "Completed"],
+    ),
+    DateTimeField(
+        name='due_date',
+        widget=DateTimeField._properties['widget'](
+            label="Due date",
+            label_msgid='indicators_label_due_date',
+            i18n_domain='indicators',
+        ),
+        required=True,
+    ),
+
+),
+)
+
+##code-section after-local-schema #fill in your manual code here
+##/code-section after-local-schema
+
+WorkItem_schema = ATContentTypeSchema.copy() + \
+    getattr(ATCTContent, 'schema', Schema(())).copy() + \
+    schema.copy()
+
+##code-section after-schema #fill in your manual code here
+finalizeATCTSchema(WorkItem_schema)
+WorkItem_schema['relatedItems'].widget.visible = {'view':'invisible', 'edit':'invisible'}
+##/code-section after-schema
+
+class WorkItem(ATCTContent, BrowserDefaultMixin):
+    """
+    """
+    security = ClassSecurityInfo()
+
+    implements(interfaces.IWorkItem)
+
+    meta_type = 'WorkItem'
+    _at_rename_after_creation = True
+
+    schema = WorkItem_schema
+
+    ##code-section class-header #fill in your manual code here
+    ##/code-section class-header
+
+    # Methods
+
+    # Manually created methods
+
+    security.declarePublic('Title')
+    def Title(self):
+        if not(self.getDue_date()):
+            return _(
+                    "title-work-get-title-none",
+                    default="Newly created work item"
+                    )
+
+        duedate = self.toLocalizedTime(self.getDue_date(), long_format=0)
+        title = _("work-work-aggregated",
+                default="Work due ${due}",  #${status} 
+                mapping = {
+                    #'status':self.getStatus(),
+                    'due':duedate,
+                    }
+                )
+        return self.translate(title)
+
+    security.declarePublic("Description")
+    def Description(self):
+        convert = getToolByName(self, 'portal_transforms').convert
+        return convert('html_to_text', self.getDescription()).getData()
+
+
+
+registerType(WorkItem, PROJECTNAME)
+# end of class WorkItem
+
+##code-section module-footer #fill in your manual code here
+##/code-section module-footer
+
+
+
