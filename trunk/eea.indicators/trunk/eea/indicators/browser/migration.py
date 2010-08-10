1296c87c94b382ea9b6231064dd5336f5aab57cb
tiberich <tiberich@24821446-e7ea-0310-81ac-b189f4507b0b>
2010-08-10 14:21:34 +0000

    Add one_state_workflow and migration script; Fixes #7496

---
+++ trunk/eea.indicators/trunk/eea/indicators/browser/migration.py
@@ -0,0 +1,51 @@
+from Acquisition import aq_base, aq_inner, aq_parent
+from Products.CMFCore.utils import getToolByName
+from Products.Five import BrowserView
+from StringIO import StringIO
+
+import logging
+logger = logging.getLogger('eea.indicators')
+
+
+class MigrateToOneStateWorkflow(BrowserView):
+    """Update the role mappings for a set of determined content types"""
+
+    wf = None
+
+    def _update_ob(self, brain):
+        ob = brain.getObject()
+        try:
+            self.wf.updateRoleMappingsFor(ob)
+        except Exception, e:
+            return
+        ob.reindexObject(idxs=['allowedRolesAndUsers'])
+
+    def __call__(self):
+
+        types = (
+                "AssessmentPart", 
+                "ExternalDataSpec", 
+                "MethodologyReference", 
+                "PolicyDocumentReference", 
+                "PolicyQuestion", 
+                "RationaleReference", 
+                "WorkItem", 
+                )
+        wf_name = "one_state_workflow"
+        
+        out = StringIO()
+
+        wftool = getToolByName(self.context, 'portal_workflow')
+        catalog = getToolByName(self.context, 'portal_catalog')
+        self.wf = wftool[wf_name]
+
+        for type_ in types:
+            res = catalog.searchResults(portal_type=type_)
+            map(self._update_ob, res)
+
+            msg = "Updated %s of type %s" % (len(res), type_)
+            logging.info(msg)
+            print >> out, msg
+
+        out.seek(0)
+        return out.read()
