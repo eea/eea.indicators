186e498e4e49373a23124a1d1ddb245cba5fc202
Alexandru Ghica <alec.ghica@eaudeweb.ro>
2010-08-23 12:23:41 +0000

    tests fixed after changing product name

--- eea/indicators/Extensions/Install.py
+++ eea/indicators/Extensions/Install.py
@@ -18,16 +18,16 @@ from Products.CMFCore.utils import getToolByName
 from eea.indicators.config import PROJECTNAME
 
 def install(self, reinstall=False):
-    """External Method to install indicators 
-    
+    """External Method to install indicators
+
     This method to install a product is kept, until something better will get
     part of Plones front end, which utilize portal_setup.
     """
     out = StringIO()
     print >> out, "Installation log of %s:" % PROJECTNAME
 
-    setuptool = getToolByName(self, 'portal_setup')   
-    importcontext = 'profile-eea.%s:default' % PROJECTNAME
+    setuptool = getToolByName(self, 'portal_setup')
+    importcontext = 'profile-%s:default' % PROJECTNAME
     setuptool.setImportContext(importcontext)
     setuptool.runAllImportSteps()
     return out.getvalue()
