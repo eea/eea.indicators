6ab291d846cd167c5cc7692e253c41d5a1388b05
tiberich <tiberich@24821446-e7ea-0310-81ac-b189f4507b0b>
2010-10-14 14:20:14 +0000

    Tagged eea.indicators 1.14.

---
+++ trunk/eea.indicators/tags/1.14/eea/indicators/content/MethodologyReference.py
@@ -0,0 +1,117 @@
+# -*- coding: utf-8 -*-
+#
+# $Id$
+#
+# Copyright (c) 2010 by ['Tiberiu Ichim']
+# Generator: ArchGenXML 
+#            http://plone.org/products/archgenxml
+#
+# GNU General Public License (GPL)
+#
+
+__author__ = """Tiberiu Ichim <unknown>"""
+__docformat__ = 'plaintext'
+
+from AccessControl import ClassSecurityInfo
+from Products.Archetypes.atapi import *
+from zope.interface import implements
+import interfaces
+from Products.ATContentTypes.content.link import ATLink
+from Products.CMFDynamicViewFTI.browserdefault import BrowserDefaultMixin
+
+from eea.indicators.config import *
+
+# additional imports from tagged value 'import'
+from Products.ATContentTypes.content.link import ATLink, ATLinkSchema
+
+##code-section module-header #fill in your manual code here
+from Products.ATContentTypes.content.schemata import finalizeATCTSchema
+from Products.CMFCore import permissions
+from Products.CMFPlone.utils import getToolByName
+##/code-section module-header
+
+schema = Schema((
+
+    StringField(
+        name='title',
+        widget=StringField._properties['widget'](
+            label='Title',
+            label_msgid='indicators_label_title',
+            i18n_domain='indicators',
+        ),
+        required=True,
+        accessor="Title",
+        required_for_published=True,
+    ),
+    TextField(
+        name='description',
+        widget=RichWidget(
+            label='Description',
+            label_msgid='indicators_label_description',
+            i18n_domain='indicators',
+        ),
+        default_content_type="text/html",
+        description="True",
+        searchable=True,
+        required=True,
+        required_for_published=True,
+        allowable_content_types=('text/plain', 'text/structured', 'text/html', 'application/msword',),
+        default_output_type="text/x-html-safe",
+        accessor="getDescription",
+    ),
+
+),
+)
+
+##code-section after-local-schema #fill in your manual code here
+##/code-section after-local-schema
+
+MethodologyReference_schema = ATLinkSchema.copy() + \
+    getattr(ATLink, 'schema', Schema(())).copy() + \
+    schema.copy()
+
+##code-section after-schema #fill in your manual code here
+MethodologyReference_schema['relatedItems'].widget.visible = {'view':'invisible', 'edit':'invisible'}
+finalizeATCTSchema(MethodologyReference_schema)
+##/code-section after-schema
+
+class MethodologyReference(ATLink, BrowserDefaultMixin):
+    """
+    """
+    security = ClassSecurityInfo()
+
+    implements(interfaces.IMethodologyReference)
+
+    meta_type = 'MethodologyReference'
+    _at_rename_after_creation = True
+
+    schema = MethodologyReference_schema
+
+    ##code-section class-header #fill in your manual code here
+    ##/code-section class-header
+
+    # Methods
+
+    # Manually created methods
+
+    security.declarePublic("Description")
+    def Description(self):
+        convert = getToolByName(self, 'portal_transforms').convert
+        return convert('html_to_text', self.getDescription()).getData()
+
+    security.declareProtected(permissions.View, 'getUrl')
+    def getUrl(self):
+        """ """
+        field = self.getField('remoteUrl')
+        return field.getAccessor(self)()
+
+
+
+registerType(MethodologyReference, PROJECTNAME)
+# end of class MethodologyReference
+
+##code-section module-footer #fill in your manual code here
+##/code-section module-footer
+
+
+
