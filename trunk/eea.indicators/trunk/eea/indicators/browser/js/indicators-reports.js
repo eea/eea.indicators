70a0d363e7c1247ad147636e3a6a049b0111b372
tiberich <tiberich@24821446-e7ea-0310-81ac-b189f4507b0b>
2010-10-20 10:33:04 +0000

    Improve javascript for ims reports; refs #7759

--- trunk/eea.indicators/trunk/eea/indicators/browser/js/indicators-reports.js
+++ trunk/eea.indicators/trunk/eea/indicators/browser/js/indicators-reports.js
@@ -1,3 +1,23 @@
+function ajaxize(target, url) {
+    $(target).load(url, function(){
+        $(".report form", target).submit(
+            function(){
+                var btn = $('input[type=');
+                var formurl = $(this).attr('action');
+                var report = $(this).parents('.report').get(0);
+                var data = $(this).serialize();
+
+                $(target).load(formurl, data, function(){ 
+                    ajaxize(target, url);
+                });
+            
+                return false;
+            }
+        );
+    });
+}
+
+
 $(document).ready(function () {
     $(".fix_btn").click(function(){
         var parent = $(this).parent();
@@ -12,8 +32,10 @@ $(document).ready(function () {
         var url = $(this).parent().find('a').attr('href');
         $(parent).append("<div class='target' style='padding:0 1em 1em 1em; " + 
                          "margin:0 1em 1em 0; border:1px solid #666'>Loading...</div>");
-        $('.target', parent).load(url);
+
+        ajaxize($('.target', parent).get(0), url);
         return false;
     });
+
 });
 
