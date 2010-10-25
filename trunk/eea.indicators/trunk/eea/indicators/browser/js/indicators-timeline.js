390f39494d6a7c4d935fab30f37cc90aefc688f0
tiberich <tiberich@24821446-e7ea-0310-81ac-b189f4507b0b>
2010-10-25 10:57:41 +0000

    Change position of warning icon in ims_timeline

--- trunk/eea.indicators/trunk/eea/indicators/browser/js/indicators-timeline.js
+++ trunk/eea.indicators/trunk/eea/indicators/browser/js/indicators-timeline.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
     $(".not-ready-anchor").each(function(){
-        var url = $(this).parent().find('a:first-child').attr('href') + '/portlet_readiness_live';
+        var url = $(this).parent().find('a:first-child').next('a').attr('href') + '/portlet_readiness_live';
         $(this).qtip({content: {
             'url':url,
             'text':'Loading, please wait...'
