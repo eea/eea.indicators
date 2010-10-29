b3698964190819803af09ee247f5cf3c67e11987
tiberich <tiberich@24821446-e7ea-0310-81ac-b189f4507b0b>
2010-10-29 08:11:59 +0000

    Tagged eea.indicators 1.21.

---
+++ trunk/eea.indicators/tags/1.21/eea/indicators/content/__init__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+#
+# $Id$
+#
+
+# Validators
+import validators
+
+# Classes
+import PolicyQuestion
+import ExternalDataSpec
+import MethodologyReference
+import Specification
+import Assessment
+import RationaleReference
+import AssessmentPart
+import PolicyDocumentReference
+import WorkItem
+
+# V1 content
+import FactSheetDocument
+import IndicatorFactSheet
+import KeyMessage
+
