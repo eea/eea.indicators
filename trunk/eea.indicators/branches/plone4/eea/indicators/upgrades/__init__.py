cb9e981302144013c1a50928a1ba3d21f39fc6f2
tiberich <tiberich@24821446-e7ea-0310-81ac-b189f4507b0b>
2011-07-14 12:34:05 +0000

    Refs #8408; added indicators migration code

--- trunk/eea.indicators/branches/plone4/eea/indicators/upgrades/__init__.py
+++ trunk/eea.indicators/branches/plone4/eea/indicators/upgrades/__init__.py
@@ -1,2 +1,28 @@
 """ Upgrades
 """
+
+from Products.Archetypes.utils import shasattr
+from Products.CMFPlone.utils import getToolByName
+from eea.versions.interfaces import IVersionEnhanced
+from zope.interface import alsoProvides, providedBy
+
+import logging
+logger = logging.getLogger('eea.indicators.migration')
+
+def assign_iversionenhanced_to_content(gstool):
+    portal = getToolByName(gstool, 'portal_url').getPortalObject()
+
+    catalog = portal.portal_catalog
+
+    ctypes = ['Specification', 'Assessment', 'IndicatorFactSheet']
+
+    for _type in ctypes:
+        res = catalog.searchResults(portal_type=_type)
+        for brain in res:
+            obj = brain.getObject()
+            if shasattr(obj, '__annotations__'):
+                if 'versionId' in obj.__annotations__.keys():
+                    if not IVersionEnhanced in providedBy(obj):
+                        alsoProvides(obj, IVersionEnhanced)
+                        logger.info(u"Added IVersionEnhanced to %s" % obj)
+
