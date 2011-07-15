be665c90c17bfe65ca325ea4de99b28e91e1d77f
tiberich <tiberich@24821446-e7ea-0310-81ac-b189f4507b0b>
2011-07-15 12:00:49 +0000

    Refs #8408; fix event handling in IMS publishing

--- trunk/eea.indicators/branches/plone4/eea/indicators/events.py
+++ trunk/eea.indicators/branches/plone4/eea/indicators/events.py
@@ -1,7 +1,8 @@
+from itertools import chain
 from Products.CMFCore.utils import getToolByName
 
 
-def publishRelatedFigures(context, dest_state):
+def syncWorkflowStateRelatedFigures(context, dest_state):
     """ Event handler"""
     wftool = getToolByName(context, "portal_workflow")
 
@@ -17,15 +18,14 @@ def publishRelatedFigures(context, dest_state):
             if figState != dest_state:
 
                 # get possible transitions for object in current state
-                for obj in ([ob] + ob.objectValues('EEAFigureFile')):
-                    actions = wftool.getActionsFor(obj)
-                    transitions = [i['transition'] 
-                            for i in actions if i.has_key('transition')]
+                for obj in chain([ob], ob.objectValues('EEAFigureFile')):
+                    workflow = wftool.getWorkflowsFor(obj)[0]
+                    transitions = workflow.transitions
 
                     # find transition that brings to the state of parent object
                     for item in filter(lambda i:i.new_state_id == dest_state, 
-                            transitions):
-                        wftool.doActionFor(obj, item.id, comment=comment)
+                            transitions.objectValues()):
+                        workflow.doActionFor(obj, item.id, comment=comment)
                         obj.reindexObject()
 
 
@@ -34,7 +34,7 @@ def handle_assessment_state_change(context, event):
     dest_state = event.workflow.transitions[event.action].new_state_id
 
     if dest_state in ['published', 'visible']:
-        publishRelatedFigures(context, dest_state)
+        syncWorkflowStateRelatedFigures(context, dest_state)
 
     if dest_state in ['visible', 'published_eionet']:
         context.allowDiscussion('off')
