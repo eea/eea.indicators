58b99ffd7105c78143a1c3a53091e4b75f641fb6
tiberich <tiberich@24821446-e7ea-0310-81ac-b189f4507b0b>
2011-08-03 08:53:52 +0000

    Tagged eea.indicators 1.47.

---
+++ trunk/eea.indicators/tags/1.47/eea/indicators/browser/interfaces.py
@@ -0,0 +1,13 @@
+"""The interfaces for the browser package"""
+
+from zope.interface import Interface 
+
+class IIndicatorsPermissionsOverview(Interface):
+    """Overview of indicators database permissions"""
+
+    #def get_users_map():
+    #    """Returns user-centered details about specs roles"""
+
+    #def get_specs_map():
+    #    """Returns spec-centered details about roles"""
+
