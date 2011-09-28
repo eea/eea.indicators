3b9ed51ce0ab23a0432e28b99e2eca83bebdbcc0
ghicaale <ghicaale@24821446-e7ea-0310-81ac-b189f4507b0b>
2011-09-28 10:09:49 +0000

    Pylint fixes.

--- trunk/eea.indicators/branches/plone4/eea/indicators/themes.py
+++ trunk/eea.indicators/branches/plone4/eea/indicators/themes.py
@@ -1,55 +1,40 @@
-"""Integration with eea.themescentre
+""" Integration with eea.themescentre
 """
 
 from Acquisition import aq_inner, aq_parent
 from eea.themecentre.interfaces import IThemeTagging
 
 class AssessmentThemes(object):
-    """Overrides the IThemeTagging adaptor for Assessments"""
+    """ Overrides the IThemeTagging adaptor for Assessments
+    """
 
     def __init__(self, context):
-        """Constructor"""
-
+        """ Constructor
+        """
         self.context = context
         self.parent = aq_parent(aq_inner(self.context))
 
     def _get_tags(self):
+        """ Use the parent tags
+        """
         return IThemeTagging(self.parent).tags
 
     def _set_tags(self, value):
-        """ do nothing on set"""
+        """ Do nothing on set
+        """
         pass
 
     tags = property(_get_tags, _set_tags)
 
     def _get_nondeprecated_tags(self):
-        """ use the parent tags """
+        """ Use the parent tags
+        """
         return IThemeTagging(self.parent).nondeprecated_tags
 
     def _set_nondeprecated_tags(self):
-        """ do nothing on set"""
+        """ Do nothing on set
+        """
         pass
 
-    nondeprecated_tags = property(_get_nondeprecated_tags, 
+    nondeprecated_tags = property(_get_nondeprecated_tags,
                                   _set_nondeprecated_tags)
-
-
-    #@property
-    #def tags(self):
-        #""" use the parent tags """
-        #return IThemeTagging(self.parent).tags
-
-    #@tags.setter
-    #def tags(self, value):
-        #""" do nothing on set"""
-        #pass
-
-    #@property
-    #def nondeprecated_tags(self):
-        #""" use the parent tags """
-        #return IThemeTagging(self.parent).nondeprecated_tags
-
-    #@nondeprecated_tags.setter
-    #def nondeprecated_tags(self, value):
-        #""" do nothing on set"""
-        #pass
