3e5207f4e1115636dfcebc99226642ecfada1805
tiberich <tiberich@24821446-e7ea-0310-81ac-b189f4507b0b>
2011-10-07 07:45:17 +0000

    Fix tests

--- trunk/eea.indicators/branches/plone4/eea/indicators/tests/test_indicators.py
+++ trunk/eea.indicators/branches/plone4/eea/indicators/tests/test_indicators.py
@@ -5,22 +5,34 @@ from Products.PloneTestCase.layer import PloneSite
 from eea.indicators.tests.base import BaseIndicatorsTestCase
 from unittest import TestSuite
 from Testing.ZopeTestCase.zopedoctest import ZopeDocFileSuite
+import doctest
+
+
+OPTIONFLAGS = (doctest.REPORT_ONLY_FIRST_FAILURE |
+               doctest.ELLIPSIS |
+               doctest.NORMALIZE_WHITESPACE)
 
 
 def test_suite():
     """test suite"""
     s1 = ZopeDocFileSuite('install.txt',
                          package='eea.indicators.doc',
-                         test_class=BaseIndicatorsTestCase)
+                         test_class=BaseIndicatorsTestCase,
+                         options=OPTIONFLAGS
+                         )
 
     s2 = ZopeDocFileSuite('contenttypes.txt',
                          package='eea.indicators.doc',
-                         test_class=BaseIndicatorsTestCase)
+                         test_class=BaseIndicatorsTestCase,
+                         options=OPTIONFLAGS
+                         )
     s2.layer = PloneSite
 
     s3 = ZopeDocFileSuite('permissions.txt',
                          package='eea.indicators.doc',
-                         test_class=BaseIndicatorsTestCase)
+                         test_class=BaseIndicatorsTestCase,
+                         options=OPTIONFLAGS
+                         )
     s3.layer = PloneSite
     return TestSuite((s1, s2, s3))
 
