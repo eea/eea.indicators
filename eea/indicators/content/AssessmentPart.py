64e340b69c5e6e00e1d4b5181076449a9d36ef8f
tiberich <tiberiuichim@users.noreply.github.com>
2011-11-30 21:01:28 +0000

    Refs #8762; WIP: fix versioning of assessmentparts by fixing relationship sorting

--- eea/indicators/content/AssessmentPart.py
+++ eea/indicators/content/AssessmentPart.py
@@ -6,21 +6,24 @@
 """
 
 from AccessControl import ClassSecurityInfo
-from Acquisition import aq_inner, aq_parent
+from Acquisition import aq_inner, aq_parent, aq_base
 from Products.ATContentTypes.content.base import ATCTContent
 from Products.ATContentTypes.content.folder import ATFolder, ATFolderSchema
 from Products.ATContentTypes.content.schemata import finalizeATCTSchema
-from Products.Archetypes.atapi import TextField, StringField, TextAreaWidget
 from Products.Archetypes.atapi import Schema, RichWidget
+from Products.Archetypes.atapi import TextField, StringField, TextAreaWidget
+from Products.Archetypes.config import REFERENCE_CATALOG
+from Products.CMFCore.utils import getToolByName
 from Products.CMFDynamicViewFTI.browserdefault import BrowserDefaultMixin
-from eea.indicators.content.base import ModalFieldEditableAware
+from eea.indicators.content import  interfaces
 from eea.indicators.content.base import CustomizedObjectFactory
+from eea.indicators.content.base import ModalFieldEditableAware
 from eea.indicators.content.interfaces import ISpecification
 from eea.indicators.content.utils import get_specific_parent
 from eea.relations.field import EEAReferenceField
 from eea.relations.widget import EEAReferenceBrowserWidget
+from plone.uuid.interfaces import IUUID
 from zope.interface import implements
-from eea.indicators.content import  interfaces
 
 
 schema = Schema((
@@ -68,12 +71,15 @@ schema = Schema((
         ),
     EEAReferenceField(
         name="relatedItems",
-        keepReferencesOnCopy=True,
         multiValued=True,
         relationship='relatesTo',
         required=True,
         validators=('one_assessment_per_question',),
-        referencesSortable=True,
+        #referencesSortable=True,
+        keepReferencesOnCopy=True,
+        accessor="get_related_items",
+        edit_accessor="get_raw_related_items",
+        mutator="set_related_items",
         widget=EEAReferenceBrowserWidget(
             label="Answers to policy question and related EEAFigures",
             description='Answers to policy question and related EEAFigures',
@@ -169,3 +175,64 @@ class AssessmentPart(ATFolder, ModalFieldEditableAware,
         figure.setThemes(themes)
         return {'obj':figure, 'subview':'edit', 'direct_edit':True}
 
+    security.declareProtected('View', 'get_related_items')
+    def get_related_items(self):
+        field = self.getField('relatedItems')
+        res = self.getRefs(relationship=field.relationship)
+        rd = {}
+        [rd.__setitem__(IUUID(r, None), r) for r in res]
+
+        if not hasattr(aq_base(self), 'at_ordered_refs'):   #not field.referencesSortable or 
+            return res
+
+        refs = self.at_ordered_refs
+        order = refs.get(field.relationship)
+        if not order:
+            return res
+
+        res = [rd[uid] for uid in order if uid in rd.keys()]
+        print "Returning %s from get_related_items()" % res
+        return res
+
+    security.declareProtected('View', 'get_raw_related_items')
+    def get_raw_related_items(self):
+        instance = self
+        field = self.getField('relatedItems')
+
+        rc = getToolByName(instance, REFERENCE_CATALOG)
+        brains = rc(sourceUID=IUUID(instance, None),
+                    relationship=field.relationship)
+        res = [b.targetUID for b in brains]
+        if not field.multiValued and not aslist:
+            if res:
+                res = res[0]
+            else:
+                res = None
+
+        if not hasattr(aq_base(instance), 'at_ordered_refs'):
+            return res
+
+        refs = instance.at_ordered_refs
+        order = refs.get(field.relationship)
+        if not order:
+            return res
+        res = [r for r in order if r in res]
+
+        print "Returning %s from get_raw_related_items()" % res
+        return res
+    
+    security.declareProtected('View', 'getRelatedItems')
+    def getRelatedItems(self):
+        return self.get_related_items()
+
+    security.declareProtected('Modify portal content', 'set_related_items')
+    def set_related_items(self, value):
+        instance = self
+        field = self.getField('relatedItems')
+        
+        field.set(self, value)
+        uids = self.get_raw_related_items()
+
+        instance.at_ordered_refs = {}
+        instance.at_ordered_refs[field.relationship] = tuple(filter(None, uids))
+        print "set raw related items to ", uids
