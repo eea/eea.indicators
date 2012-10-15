f793386b4a78d2de3a407356aaf213a86e26db14
tiberich <tiberich@24821446-e7ea-0310-81ac-b189f4507b0b>
2012-10-15 14:11:33 +0000

    Refs #7965; code cleanup

--- trunk/eea.indicators/trunk/eea/indicators/content/marshaller.py
+++ trunk/eea.indicators/trunk/eea/indicators/content/marshaller.py
@@ -1,8 +1,8 @@
-from Products.Archetypes.interfaces import IField
 from eea.rdfmarshaller.archetypes.fields import ATField2Surf
 from eea.rdfmarshaller.interfaces import ISurfSession
 from zope.component import adapts
 from zope.interface import Interface
+
 
 class CodesField2Surf(ATField2Surf):
     """rdfmarshaller field adapter for the codes field
@@ -10,6 +10,8 @@ class CodesField2Surf(ATField2Surf):
     adapts(IField, Interface, ISurfSession)
 
     def value(self):
+        """return value suitable for surf
+        """
         if not self.value:
             return None
         return ["%s%s" % (v['set'], v['code'])
