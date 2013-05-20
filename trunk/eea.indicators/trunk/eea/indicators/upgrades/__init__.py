d43347714f1e6230c9d570df88b8d4383fb23930
tiberich <tiberich@24821446-e7ea-0310-81ac-b189f4507b0b>
2013-05-20 10:31:54 +0000

    Refs #14341 EEA; added an upgrade step to cleanup assessmentparts - daviz annotations

--- trunk/eea.indicators/trunk/eea/indicators/upgrades/__init__.py
+++ trunk/eea.indicators/trunk/eea/indicators/upgrades/__init__.py
@@ -5,9 +5,12 @@ from Products.Archetypes.utils import shasattr
 from Products.CMFPlone.utils import getToolByName
 from eea.versions.interfaces import IVersionEnhanced
 from zope.interface import alsoProvides, providedBy
-
 import logging
+import transaction
+
+
 logger = logging.getLogger('eea.indicators.migration')
+
 
 def assign_iversionenhanced_to_content(gstool):
     """ Update IMS content to provide IVersionEnhanced if they should
@@ -28,3 +31,49 @@ def assign_iversionenhanced_to_content(gstool):
                         alsoProvides(obj, IVersionEnhanced)
                         logger.info(u"Added IVersionEnhanced to %s" % obj)
 
+
+def cleanup_assessmentparts(gstool):
+    """ Cleanup AssessmentPart annotation that contains missing daviz info
+    """
+    portal = getToolByName(gstool, 'portal_url').getPortalObject()
+
+    catalog = portal.portal_catalog
+    res = catalog.searchResults(portal_type='AssessmentPart')
+    uids_cat = getToolByName(portal, 'uid_catalog')
+
+    logger.info("Starting cleanup of assessmentpart-daviz annotation")
+    i = 0
+    for b in res:
+        obj = b.getObject()
+        annot = obj.__annotations__.get('DAVIZ_CHARTS', {})
+
+        for uid in annot.keys():
+            brains = uids_cat.searchResults(UID=uid)
+            if not brains:
+                msg = "Couldn't find object for brain with UID %s, "\
+                      "deleting from assessmentpart %s" % (uid,
+                      obj.absolute_url())
+                logger.info(msg)
+                del annot[uid]
+                annot._p_changed = True
+                obj._p_changed = True
+                i += 1
+                continue
+
+            daviz = brains[0].getObject()
+            if daviz is None:   #brain does not lead to object?
+                msg = "Couldn't find object for brain with UID %s, "\
+                      "deleting from assessmentpart %s" % (uid,
+                      obj.absolute_url())
+                logger.info(msg)
+                del annot[uid]
+                annot._p_changed = True
+                obj._p_changed = True
+                i += 1
+
+        if (i % 20) == 0:
+            transaction.savepoint() #savepoint on every 20 changes
+
+    transaction.commit()
+
+    logger.info("End cleanup of assessmentpart-daviz annotation")
