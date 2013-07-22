02505e489d6c3cf2adbaf24ef4a9b40573539b24
tiberich <tiberiuichim@users.noreply.github.com>
2013-07-22 11:32:41 +0000

    Refs #14871 EEA; added script to download esms.xml files

---
+++ eea/indicators/scripts/download_esms.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+""" Save the links file and this script to the same directory. 
+
+Execute as python name_of_script.py path_to_links_file.txt
+It will download all links as xml text files to the current directory
+"""
+
+import sys
+import urllib
+
+
+if __name__ == "__main__":
+    if not len(sys.argv) == 2:
+        print "You need to provide a path to the input filename"
+        sys.exit(1)
+
+    inp = sys.argv[1]
+    with open(inp) as f:
+        links = f.readlines()
+
+    for l in links:
+        if l.strip():
+            fname = l.split("/")[-3] + '.xml'
+            print "Saving %s from %s..." % (fname, l.strip()),
+            req = urllib.urlopen(l)
+            data = req.read()
+
+            #links are http://site/path/indicator-id/assessment-id/@@esms.xml
+            with open(fname, 'w') as f:
+                f.write(data)
+            print "done."
+
