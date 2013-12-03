b186f31bbcfad7e072f604c379d3909d73f1d48e
szabozo0 <zotya@users.noreply.github.com>
2013-12-03 15:34:53 +0000

    Refs #17453; Disable resize for IE with version older than 9

--- eea/indicators/browser/js/daviz-inline-resize.js
+++ eea/indicators/browser/js/daviz-inline-resize.js
@@ -1,5 +1,11 @@
 jQuery(document).ready(function($){
-    var resizeButton = "<a class='standardButton googlechart-inline-resize'>Resize chart</div>";
+    if (/MSIE (\d+\.\d+);/.test(navigator.userAgent)){
+        var ieversion=RegExp.$1;
+        if (ieversion<9){
+            return;
+        }
+    }
+    var resizeButton = "<a class='standardButton googlechart-inline-resize'>Resize chart</a>";
 
     jQuery.each(jQuery(".embedded-daviz-visualization .standardButton"), function(){
         if ((jQuery(this).closest("dd").find("div.figure-chart-live").length > 0) &&
