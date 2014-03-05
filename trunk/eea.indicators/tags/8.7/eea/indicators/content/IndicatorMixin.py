5e397a704fbef078d48ff68b3ba7de1f3f5c9f0d
ichimdav <ichimdav@24821446-e7ea-0310-81ac-b189f4507b0b>
2014-03-05 12:35:17 +0000

    Tagged eea.indicators 8.7.

---
+++ trunk/eea.indicators/tags/8.7/eea/indicators/content/IndicatorMixin.py
@@ -0,0 +1,90 @@
+"""A mixin class for objects that function as indicators"""
+
+import logging
+from Products.CMFCore.utils import getToolByName
+from AccessControl import ClassSecurityInfo
+#from eea.versions.versions import get_versions_api
+from eea.versions.interfaces import IGetVersions
+from Missing import Value as MissingValue
+
+logger = logging.getLogger("eea.indicators.content")
+
+class IndicatorMixin(object):
+    """A mixin for Specification and IndicatorFactSheet"""
+
+    security = ClassSecurityInfo()
+
+    security.declarePublic("has_duplicated_code")
+    def has_duplicated_code(self):
+        """Return True if this specification has the wrong version id"""
+        duplicates = self.get_duplicated_codes()
+        return bool(duplicates)
+
+    security.declarePublic("get_duplicated_codes")
+    def get_duplicated_codes(self):
+        """Returns codes that are duplicated by some other indicator"""
+
+        versions = [v.UID() for v in IGetVersions(self).versions()]
+
+        search = getToolByName(self, 'portal_catalog').searchResults
+        codes = self.getCodes()
+        self_UID = self.UID()
+
+        #We want to see if there are other specs with the same code
+        #that are not versions of this object.
+        #if any version has the same UID as the checked object,
+        #then we consider all versions to be the same as the object
+
+        duplicated_codes = []
+        for code in codes:
+            code = code['set'] + code['code']
+            brains = search(portal_type="Specification", get_codes=[code])
+            #brains += cat(portal_type="IndicatorFactSheet", get_codes=[code])
+
+            not_same = [b for b in brains if (b.UID not in versions) 
+                                         and (b.UID != self_UID)]
+
+            # now we filter the specification based on their versionId; 
+            # we don't want to report all specifications in the versionId group
+            _d = {}
+            for b in not_same:
+                if b.getVersionId == MissingValue: #this is infrequent
+                    logger.warn( "Missing versionid value: %s", b.getObject())
+                    continue
+                _d[b.getVersionId.strip()] = b
+
+            if _d:
+                duplicated_codes.append((code, _d.values()))
+
+        return duplicated_codes
+
+    security.declarePublic("get_diff_vers_setcode")
+    def get_diff_vers_setcode(self):
+        """Returns a list of versions of this Spec that have
+           a different main setcode
+        """
+        diff = []
+        codes = self.getCodes()
+        if not codes:
+            return diff
+
+        code = codes[0]
+        for v in IGetVersions(self).versions():
+            if v.getCodes() and v.getCodes()[0] != code:
+                diff.append(v)
+        return diff
+
+    security.declarePublic('getCandidateFixedCode')
+    def getCandidateFixedCode(self, spec):
+        """Returns codes that a spec should get to have a similar main
+           setcode to context
+        """
+        main = self.getCodes()[0]
+        other = spec.getCodes()
+
+        return [main] + [c for c in other if c != main]
+
+    security.declarePublic('format_codes')
+    def format_codes(self, codes):
+        """format codes"""
+        return ", ".join(["%s%s" % (s['set'], s['code']) for s in codes])
---
+++ trunk/eea.indicators/tags/8.7/eea/indicators/skins/indicators_templates/simple_content_edit_impl.py
@@ -0,0 +1,85 @@
+## Script (Python) "simple_content_edit_impl"
+##title=Edit content
+##bind container=container
+##bind context=context
+##bind namespace=
+##bind script=script
+##bind subpath=traverse_subpath
+##parameters=state, id=''
+##
+
+from Products.Archetypes import PloneMessageFactory as _
+from Products.Archetypes.utils import addStatusMessage
+from Products.CMFCore.utils import getToolByName
+
+REQUEST = context.REQUEST
+old_id = context.getId()
+
+#this is probably not needed
+specific_field = context.REQUEST.form.get('specific_field')
+state.set(specific_field=specific_field)
+
+try:
+    new_context = context.portal_factory.doCreate(context, id)
+except AttributeError:
+    # Fallback for AT + plain CMF where we don't have a portal_factory
+    new_context = context
+
+new_context.simpleProcessForm(REQUEST=REQUEST)
+
+# Get the current language and put it in request/LANGUAGE
+form = REQUEST.form
+if form.has_key('current_lang'):
+    form['language'] = form.get('current_lang')
+
+fieldset = REQUEST.get('fieldset', None)
+schemata = new_context.Schemata()
+
+env = state.kwargs
+reference_source_url = env.get('reference_source_url')
+if reference_source_url is not None:
+    reference_source_url = env['reference_source_url'].pop()
+    reference_source_field = env['reference_source_field'].pop()
+    reference_source_fieldset = env['reference_source_fieldset'].pop()
+    portal = context.portal_url.getPortalObject()
+    reference_obj = portal.restrictedTraverse(reference_source_url)
+
+    # XXX disabled mark creation flag
+    ## context.remove_creation_mark(old_id)
+
+    kwargs = {
+        'status':'success_add_reference',
+        'context':reference_obj,
+        'fieldset':reference_source_fieldset,
+        'field':reference_source_field,
+        'reference_focus':reference_source_field,
+        }
+    return state.set(**kwargs)
+
+if state.errors:
+    import pdb; pdb.set_trace()
+    errors = state.errors
+    s_items = [(s, schemata[s].keys()) for s in schemata.keys()]
+    fields = []
+    portal_status_message = "Error occurred when adding relation"
+    for s, f_names in s_items:
+        for f_name in f_names:
+            fields.append((s, f_name))
+    for s_name, f_name in fields:
+        if errors.has_key(f_name):
+            REQUEST.set('fieldset', s_name)
+            addStatusMessage(REQUEST, portal_status_message)
+            return state.set(
+                status='failure',
+                context=new_context,)
+
+if not state.errors:
+    from Products.Archetypes.utils import transaction_note
+    transaction_note('Edited %s %s at %s' % (new_context.meta_type,
+                                             new_context.title_or_id(),
+                                             new_context.absolute_url()))
+
+return state.set(status='success',
+                 context=new_context,
+                 )
+
