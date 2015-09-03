1e4a020c0b91780df0d33ed4ce6c0fb6bdca6779
ghicaale <ghicaale@24821446-e7ea-0310-81ac-b189f4507b0b>
2015-09-03 09:58:56 +0000

    Tagged eea.indicators 10.4.

---
+++ trunk/eea.indicators/tags/10.4/eea/indicators/tests/test_indicators.py
@@ -0,0 +1,46 @@
+""" Test suites for eea.indicators
+"""
+
+from Products.PloneTestCase.layer import PloneSite
+from eea.indicators.tests.base import BaseIndicatorsTestCase
+from unittest import TestSuite
+from Testing.ZopeTestCase.zopedoctest import ZopeDocFileSuite
+import doctest
+
+
+OPTIONFLAGS = (doctest.REPORT_ONLY_FIRST_FAILURE |
+               doctest.ELLIPSIS |
+               doctest.NORMALIZE_WHITESPACE)
+
+
+def test_suite():
+    """test suite"""
+    s1 = ZopeDocFileSuite('install.txt',
+                         package='eea.indicators.doc',
+                         test_class=BaseIndicatorsTestCase,
+                         #options=OPTIONFLAGS
+                         )
+
+    s2 = ZopeDocFileSuite('contenttypes.txt',
+                         package='eea.indicators.doc',
+                         test_class=BaseIndicatorsTestCase,
+                         #options=OPTIONFLAGS
+                         )
+    s2.layer = PloneSite
+
+    s3 = ZopeDocFileSuite('permissions.txt',
+                         package='eea.indicators.doc',
+                         test_class=BaseIndicatorsTestCase,
+                         #options=OPTIONFLAGS
+                         )
+    s3.layer = PloneSite
+
+    s4 = ZopeDocFileSuite('marshaller.txt',
+                         package='eea.indicators.doc',
+                         test_class=BaseIndicatorsTestCase,
+                         #options=OPTIONFLAGS
+                         )
+    s4.layer = PloneSite
+
+    return TestSuite((s1, s2, s3, s4))
+
