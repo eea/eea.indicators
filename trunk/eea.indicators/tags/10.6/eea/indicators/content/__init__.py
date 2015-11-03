226ac2c161b79432a349f33b7015f7d1a1eec7c1
ghicaale <ghicaale@24821446-e7ea-0310-81ac-b189f4507b0b>
2015-11-03 15:46:45 +0000

    Tagged eea.indicators 10.6.

---
+++ trunk/eea.indicators/tags/10.6/eea/indicators/content/__init__.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+#
+# $Id$
+#
+
+""" The content types package
+"""
+from Products.validation import validation
+from Products.Archetypes.atapi import registerType
+
+from eea.indicators.config import PROJECTNAME
+
+from eea.indicators.content.validators import (
+    UniquePolicyDocTitleValidator,
+    UniquePolicyDocUrlValidator,
+    OneAssessmentPartPerQuestionValidator,
+    FrequencyUpdateValidator,
+    TimeOfYearValidator,
+)
+
+# Validators
+validation.register(
+    UniquePolicyDocTitleValidator('unique_policy_title_validator'))
+validation.register(
+    UniquePolicyDocUrlValidator('unique_policy_url_validator'))
+validation.register(
+    OneAssessmentPartPerQuestionValidator('one_assessment_per_question'))
+validation.register(
+    FrequencyUpdateValidator('validate_frequency_years'))
+validation.register(TimeOfYearValidator('validate_time_of_year'))
+
+from eea.indicators.content.PolicyQuestion import PolicyQuestion
+from eea.indicators.content.ExternalDataSpec import ExternalDataSpec
+from eea.indicators.content.MethodologyReference import MethodologyReference
+from eea.indicators.content.Specification import Specification
+from eea.indicators.content.Assessment import Assessment
+from eea.indicators.content.RationaleReference import RationaleReference
+from eea.indicators.content.AssessmentPart import AssessmentPart
+from eea.indicators.content.WorkItem import WorkItem
+from eea.indicators.content.FactSheetDocument import FactSheetDocument
+from eea.indicators.content.IndicatorFactSheet import IndicatorFactSheet
+from eea.indicators.content.KeyMessage import KeyMessage
+from eea.indicators.content.PolicyDocumentReference import (
+    PolicyDocumentReference,
+)
+
+def register():
+    """ Register custom content
+    """
+
+    # Content
+    registerType(PolicyQuestion, PROJECTNAME)
+    registerType(ExternalDataSpec, PROJECTNAME)
+    registerType(MethodologyReference, PROJECTNAME)
+    registerType(Specification, PROJECTNAME)
+    registerType(Assessment, PROJECTNAME)
+    registerType(RationaleReference, PROJECTNAME)
+    registerType(AssessmentPart, PROJECTNAME)
+    registerType(PolicyDocumentReference, PROJECTNAME)
+    registerType(WorkItem, PROJECTNAME)
+
+    # V1 content
+    registerType(FactSheetDocument, PROJECTNAME)
+    registerType(IndicatorFactSheet, PROJECTNAME)
+    registerType(KeyMessage, PROJECTNAME)
