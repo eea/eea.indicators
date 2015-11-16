5882576856f4c567bcba40cbdd6787a5854ae0a2
ghicaale <ghicaale@24821446-e7ea-0310-81ac-b189f4507b0b>
2015-11-16 12:38:56 +0000

    Tagged eea.indicators 10.9.

---
+++ trunk/eea.indicators/tags/10.9/eea/indicators/browser/interfaces.py
@@ -0,0 +1,31 @@
+"""The interfaces for the browser package"""
+
+from zope.interface import Interface
+
+class IIndicatorsPermissionsOverview(Interface):
+    """Overview of indicators database permissions"""
+
+    #def get_users_map():
+    #    """Returns user-centered details about specs roles"""
+
+    #def get_specs_map():
+    #    """Returns spec-centered details about roles"""
+
+
+class IIndicatorUtils(Interface):
+    """This view can provide various utilities"""
+    #str_to_id = Attribute(u"String to Id conversion")
+
+    def str_to_id():
+        """Convert an ordinary string (maybe title) to something that
+           can be used as a DOM element ID
+        """
+
+    def field_has_value(fieldname, context):
+        """Return True if the given field has a value
+           for the given context object
+        """
+
+
+class IIMSBaseView(Interface):
+    """Marker interface for IMS views"""
