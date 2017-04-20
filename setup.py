c5327cd6c1f5105bb7f46120f4e84d692516c721
eduard-fironda <eduard.fironda@eaudeweb.ro>
2017-04-20 18:38:39 +0300

    updated package information
    * Change: updated package information
    [eduard-fironda]

--- setup.py
+++ setup.py
@@ -15,15 +15,23 @@ setup(
                   open(os.path.join("docs", "HISTORY.txt")).read(),
  url="https://svn.eionet.europa.eu/projects/"
      "Zope/browser/trunk/eea.indicators",
- classifiers=[
-     "Framework :: Plone",
-     "Programming Language :: Python",
-     "Topic :: Software Development :: Libraries :: Python Modules",
-   ],
- keywords='EEA indicators ims indicatorsmanagementsystem',
- author=('Alec Ghica (Eaudeweb), Tiberiu Ichim (Eaudeweb), '
-         'Antonio De Marinis (EEA), European Environment Agency'),
- author_email='webadmin@eea.europa.eu',
+      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+          "Framework :: Zope2",
+          "Framework :: Plone",
+          "Framework :: Plone :: 4.0",
+          "Framework :: Plone :: 4.1",
+          "Framework :: Plone :: 4.2",
+          "Framework :: Plone :: 4.3",
+          "Programming Language :: Zope",
+          "Programming Language :: Python",
+          "Programming Language :: Python :: 2.7",
+          "Topic :: Software Development :: Libraries :: Python Modules",
+          "License :: OSI Approved :: GNU General Public License (GPL)",
+      ],
+ keywords='EEA indicators Add-ons Plone Zope',
+ author='European Environment Agency: IDM2 A-Team',
+ author_email='eea-edw-a-team-alerts@googlegroups.com',
  license='GPL',
  packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
  namespace_packages=['eea'],
