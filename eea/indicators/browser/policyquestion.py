9237e91af049818b489025b2f4bfe0dd1880db95
Valentin Dumitru <valentin.dumitru@eaudeweb.ro>
2018-01-26 16:58:15 +0000

    Custom view for the PolicyQuestion, shows Key Items from the current version of the assessment report ('related item') Updated profile with some changes already in the ZMI

---
+++ eea/indicators/browser/policyquestion.py
@@ -0,0 +1,35 @@
+from Products.Five import BrowserView
+from zope.component import queryAdapter
+
+from eea.versions.interfaces import IGetVersions
+
+class KeyMessages(BrowserView):
+    """ search for the latest version of the first related item and return
+    its key message to be displayed in the question view """
+
+    def get_versions(self):
+        related = [rel for rel in self.context.getBRefs() if rel is not None]
+        if related:
+            related = related[0]
+            return queryAdapter(related, IGetVersions)
+
+    def get_assessment(self):
+        versions = self.get_versions()
+        if versions:
+            return versions.latest_version()
+
+    def key_messages(self):
+        assessment = self.get_assessment()
+        if assessment:
+            return assessment.getKey_message()
+
+    def assessment_url(self):
+        versions = self.get_versions()
+        if versions:
+            return versions.getLatestVersionUrl()
+
+    def assessment_date(self):
+        versions = self.get_versions()
+        if versions:
+            return self.context.toLocalizedTime(
+                self.get_assessment().modified())
